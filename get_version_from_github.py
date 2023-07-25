1fa49f50268945f21c64a14b8259c9503e668c1f
agordillo <agordillo@translations.com>
2023-07-25 15:35:45 +0200

    changed from pandas to csv (much more lightweight) and updated compare_versions to check if version is None

--- get_version_from_github.py
+++ get_version_from_github.py
@@ -1,18 +1,29 @@
-# import pandas as pd
 import csv
-GITHUB_CSV = r"https://raw.githubusercontent.com/TransperfectTPT/versions/main/versions.csv"
+import urllib.request
+GITHUB_CSV = "https://raw.githubusercontent.com/TransperfectTPT/versions/main/versions.csv"
 
 
 def get_version(toolname):
-    df = pd.read_csv(GITHUB_CSV, header=0, index_col=0)
-    gh_version = df.loc[toolname, "version"]
-    print(f'Tool: {toolname}, GitHub version: {gh_version}')
-    return gh_version
+    # Instead of pandas, we will use the csv module
+    response = urllib.request.urlopen(GITHUB_CSV)
+    lines = [l.decode('utf-8') for l in response.readlines()]
+    # print(lines)
+    cr = csv.reader(lines)
+    for row in cr:
+        # list indices must be integer or slices, not str
+        if row[0] == toolname:
+            gh_version = row[1]
+            print(f'Tool: {toolname}, GitHub version: {gh_version}')
+            return gh_version
 
 
 def compare_versions(toolname, current_version):
     try:
         gh_version = get_version(toolname)
+
+        if gh_version is None:
+            print(f"Tool name not found: {toolname}")
+            raise KeyError(f"Tool name not found: {toolname}")
 
         if gh_version > current_version:
             print(f"New version available: {gh_version}")
@@ -25,4 +36,4 @@ def compare_versions(toolname, current_version):
 
 
 if __name__ == '__main__':
-    get_version("Tool example 1")
+    compare_versions("Storyline Exports Wizard", "1.0")
